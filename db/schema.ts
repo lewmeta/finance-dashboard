3779c13deec8f019556f62e96aa1c0a65a7e675c
Lewis <lewis@MacBook-Pro-Lewis.local>
2024-11-08 12:01:04 +0300

    Configured Drizzle

---
+++ db/schema.ts
@@ -0,0 +1,17 @@
+// import {z} from 'zod'
+// import { createInserSchema} from 'drizzle-zod'
+// import {relations } from 'drizzle-orm'
+import {
+    // integer,
+    pgTable,
+    text,
+    // timestamp,
+} from 'drizzle-orm/pg-core'
+
+export const accounts = pgTable('accounts', {
+    id: text('id').primaryKey(),
+    pladiId: text('plaid_id'),
+    name: text('name').notNull(),
+    userId: text('user_id').notNull(),
+});
+
