32fce34322e9ce5a4a24c61bedb15b2c696dd928
Lewis <lewis@MacBook-Pro-Lewis.local>
2024-11-08 14:02:40 +0300

    Added accounts get

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+'use client'
 
 export default function Home() {
+  
   return (
-    <UserButton
-      signInUrl="/"
-    />
+    <div>
+      Home page
+    </div>
   );
 }
