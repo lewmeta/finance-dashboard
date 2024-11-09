2e7ebfc7451a14e49dface4f9b4d7fd3478bc2b7
Lewis <lewis@MacBook-Pro-Lewis.local>
2024-11-09 12:35:32 +0300

    added real data and skeleton

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/data-table";
 
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
+import { Skeleton } from "@/components/ui/skeleton";
 import {
     Card,
     CardContent,
@@ -14,24 +17,29 @@ import {
     CardTitle
 } from "@/components/ui/card";
 
-import { columns, Payment } from "./columns";
+import { columns } from "./columns";
 
-const data: Payment[] = [
-    {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-    },
-    {
-        id: "728ed52sd",
-        amount: 10,
-        status: "processing",
-        email: "l@example.com",
-    },
-];
 const AccountsPage = () => {
     const newAccount = useNewAccount();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton  className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin"/>
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -50,10 +58,10 @@ const AccountsPage = () => {
                 </CardHeader>
                 <CardContent>
                     <DataTable
-                        filterKey="email"
+                        filterKey="name"
                         columns={columns}
-                        data={data}
-                        onDelete={() => {}}
+                        data={accounts}
+                        onDelete={() => { }}
                         disabled
                     />
                 </CardContent>
