15764304286e094ea76b5b8f9bc2e41a78401f05
Lewis <lewis@MacBook-Pro-Lewis.local>
2024-11-09 16:47:56 +0300

    Added a single delete

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { Edit, MoreHorizontal } from 'lucide-react';
+import { Edit, MoreHorizontal, Trash2 } from 'lucide-react';
 
 import { useOpenAccount } from '@/features/accounts/hooks/use-open-account';
+import { useDeleteAccount } from '@/features/accounts/api/use-delete-account';
+import { useConfirm } from '@/hooks/use-confirm';
 
 import { Button } from '@/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
@@ -12,9 +14,24 @@ type Props = {
 }
 
 export const Actions = ({ id }: Props) => {
+    const [ConfirmDialog, confirm] = useConfirm(
+        'Are you sure',
+        'You are about to delete this transaction!'
+    );
+    const deleteMutation = useDeleteAccount(id);
     const { onOpen } = useOpenAccount();
+
+    const handleDelete = async () => {
+        const ok = await confirm();
+
+        if (ok) {
+            deleteMutation.mutate();
+        }
+    }
+
     return (
         <>
+        <ConfirmDialog/>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button
@@ -26,11 +43,19 @@ export const Actions = ({ id }: Props) => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align='end'>
                     <DropdownMenuItem
-                        disabled={false}
+                        disabled={deleteMutation.isPending}
                         onClick={() => onOpen(id)}
                     >
                         <Edit className='size-4 mr-2' />
                         Edit
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                        disabled={deleteMutation.isPending}
+                        onClick={handleDelete}
+
+                    >
+                        <Trash2 className='size-4 mr-2' />
+                        Delete
                     </DropdownMenuItem>
 
                 </DropdownMenuContent>
--- features/accounts/components/edit-accout-sheet.tsx
+++ features/accounts/components/edit-accout-sheet.tsx
@@ -7,6 +7,9 @@ import { useOpenAccount } from '@/features/accounts/hooks/use-open-account'
 import { useGetAccount } from '@/features/accounts/api/use-get-account'
 import { insertAccountSchema } from '@/db/schema'
 import { useEditAccount } from '@/features/accounts/api/use-edit-account'
+import { useDeleteAccount } from '@/features/accounts/api/use-delete-account'
+import { useConfirm } from '@/hooks/use-confirm'
+
 import {
     SheetHeader,
     SheetDescription,
@@ -23,13 +26,20 @@ const formSchema = insertAccountSchema.pick({
 type FormValues = z.input<typeof formSchema>
 
 export const EditAccountSheet = () => {
-    const { isOpen, onClose, id } = useOpenAccount()
+    const { isOpen, onClose, id } = useOpenAccount();
+
+    const [ConfirmDialog, confirm] = useConfirm(
+        'Are you sure?',
+        'You are about to delete this transaction'
+    )
 
     const accountQuery = useGetAccount(id);
-    const editMutation = useEditAccount(id)
+    const editMutation = useEditAccount(id);
+    const deleteMutation = useDeleteAccount(id);
 
-    const isPending = editMutation
-        .isPending
+    const isPending =
+        editMutation.isPending ||
+        deleteMutation.isPending;
 
     const isLoading = accountQuery.isLoading;
 
@@ -41,38 +51,54 @@ export const EditAccountSheet = () => {
         });
     };
 
+    const onDelete = async () => {
+        const ok = await confirm();
+
+        if (ok) {
+            deleteMutation.mutate(undefined, {
+                onSuccess: () => {
+                    onClose()
+                }
+            })
+        }
+    }
+
     const defaultValues = accountQuery.data ?
         { name: accountQuery.data.name }
         :
         { name: '' }
 
     return (
-        <Sheet open={isOpen} onOpenChange={onClose}>
-            <SheetContent>
-                <SheetHeader className="space-y-4 text-start">
-                    <SheetTitle>
-                        Edit account
-                    </SheetTitle>
-                    <SheetDescription>
-                        Edit an existing account.
-                    </SheetDescription>
-                </SheetHeader>
-                {isLoading
-                    ? (
-                        <div className='absolute inset-0 flex items-center justify-center'>
-                            <Loader2 className='size-4 text-muted-foreground animate-spin' />
-                        </div>
-                    ) :
-                    (
-                        <AccountForm
-                            id={id}
-                            onSubmit={onSubmit}
-                            disabled={isPending}
-                            defaultValues={defaultValues}
-                        />
-                    )
-                }
-            </SheetContent>
-        </Sheet>
+        <>
+            <ConfirmDialog />
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent>
+                    <SheetHeader className="space-y-4 text-start">
+                        <SheetTitle>
+                            Edit account
+                        </SheetTitle>
+                        <SheetDescription>
+                            Edit an existing account.
+                        </SheetDescription>
+                    </SheetHeader>
+                    {isLoading
+                        ? (
+                            <div className='absolute inset-0 flex items-center justify-center'>
+                                <Loader2 className='size-4 text-muted-foreground animate-spin' />
+                            </div>
+                        ) :
+                        (
+                            <AccountForm
+                                id={id}
+                                onSubmit={onSubmit}
+                                disabled={isPending}
+                                defaultValues={defaultValues}
+                                onDelete={onDelete}
+                            />
+                        )
+                    }
+                </SheetContent>
+            </Sheet>
+        </>
     )
 }
