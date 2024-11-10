c5dc35a35952614636b31465252a9868662b8ce1
lewmeta <dev.lewismeta@gmail.com>
2024-11-10 18:21:56 +0300

    Added hooks to handle the transactions CRUD endpoints

---
+++ features/transaction/api/use-delete-transaction.ts
@@ -0,0 +1,38 @@
+import { toast } from "sonner";
+
+import { InferResponseType, InferRequestType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$delete"]
+>;
+
+export const useDeleteTransaction = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<
+    ResponseType, 
+    Error
+  >({
+    mutationFn: async () => {
+      const response = await client.api.transactions[":id"]["$delete"]({
+        param: { id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Transaction deleted!");
+      queryClient.invalidateQueries({ queryKey: ["transaction", {id}] });
+      queryClient.invalidateQueries({ queryKey: ["transactions"] });
+
+      // TODO: Invalidate summery and transactions
+    },
+    onError: () => {
+      toast.error("Failed to delete transaction!");
+    },
+  });
+
+  return mutation;
+};
