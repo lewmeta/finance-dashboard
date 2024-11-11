8107d020d16909a172269086d032bb6bc9063d9e
lewmeta <dev.lewismeta@gmail.com>
2024-11-11 13:05:20 +0300

    Added react select

--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -3,38 +3,58 @@ import { Trash } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-import { insertAccountSchema } from '@/db/schema'
+import { insertTransactionSchema } from '@/db/schema'
+
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { Select } from '@/components/select'
 import {
     Form,
     FormControl,
     FormField,
     FormItem,
     FormLabel,
-    FormMessage
 } from '@/components/ui/form'
 
-const formSchema = insertAccountSchema.pick({
-    name: true,
-})
+const formSchema = z.object({
+    date: z.coerce.date(),
+    accountId: z.string(),
+    categoryId: z.string().nullable().optional(),
+    payee: z.string(),
+    amount: z.string(),
+    notes: z.string().nullable().optional(),
+});
 
-type FormValues = z.input<typeof formSchema>
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const apiSchema = insertTransactionSchema.omit({
+    id: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+type ApiFormValues = z.input<typeof apiSchema>;
 
 type Props = {
     id?: string;
     defaultValues?: FormValues;
-    onSubmit: (values: FormValues) => void;
+    onSubmit: (values: ApiFormValues) => void;
     onDelete?: () => void;
     disabled?: boolean;
+    accountOptions: { label: string; value: string; }[];
+    categoryOptions: { label: string; value: string }[];
+    onCreateAccount: (name: string) => void;
+    onCreateCategory: (name: string) => void;
 }
 
-export const AccountForm = ({
+export const TransactionForm = ({
     id,
     defaultValues,
     disabled,
     onSubmit,
-    onDelete
+    onDelete,
+    accountOptions,
+    categoryOptions,
+    onCreateAccount,
+    onCreateCategory
 }: Props) => {
     const form = useForm<FormValues>({
         resolver: zodResolver(formSchema),
@@ -42,7 +62,7 @@ export const AccountForm = ({
     })
 
     const handleSubmit = (values: FormValues) => {
-        onSubmit(values)
+        console.log({ values })
     }
 
     const handleDelete = () => {
@@ -56,15 +76,19 @@ export const AccountForm = ({
                 className='space-y-4'
             >
                 <FormField
-                    name='name'
+                    name='accountId'
                     control={form.control}
                     render={({ field }) => (
                         <FormItem>
-                            <FormLabel>Name</FormLabel>
+                            <FormLabel>Account</FormLabel>
                             <FormControl>
-                                <Input
-                                    placeholder='e.g Cash, Bank, Credit Card'
-                                    {...field}
+                                <Select
+                                    placeholder='Select an account'
+                                    options={accountOptions}
+                                    onCreate={onCreateAccount}
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                    disabled={disabled}
                                 />
                             </FormControl>
                         </FormItem>
