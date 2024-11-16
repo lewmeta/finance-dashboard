7e87b93b1ec33c36c2165673c6f4ac62ac055fc0
lewmeta <dev.lewismeta@gmail.com>
2024-11-16 15:24:30 +0300

    added a category column

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -11,6 +11,7 @@ import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Actions } from "./actions"
+import { AccountColumn } from "./account-column"
 
 export type ResponseType = InferResponseType<typeof client.api.transactions.$get, 200>['data'][0];
 
@@ -114,9 +115,9 @@ export const columns: ColumnDef<ResponseType>[] = [
       const amount = parseFloat(row.getValue('amount'));
 
       return (
-        <Badge 
-        variant={amount < 0 ? 'destructive' : 'default'}
-        className='text-sm font-medium px-3.5 py-2.5'
+        <Badge
+          variant={amount < 0 ? 'destructive' : 'primary'}
+          className='text-sm font-medium px-3.5 py-2.5'
         >{formatCurrency(amount)}</Badge>
       )
     }
@@ -136,7 +137,10 @@ export const columns: ColumnDef<ResponseType>[] = [
     },
     cell: ({ row }) => {
       return (
-        <span>{row.original.account}</span>
+        <AccountColumn
+          account={row.original.account}
+          accountId={row.original.accountId}
+        />
       )
     }
   },
